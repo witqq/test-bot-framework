9a04328b2c60e17a97022ad1911c49c865967f25
belyi <belyi@greendatasoft.ru>
2017-09-20 03:11:47 +0500

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 8080, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: "9b62c71f-1af3-42a7-afd3-97da6768a00c",
+    appPassword: "0MTXpGxEiX5vz6qSbEmLDCh"
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+server.get('/', (req, res) => {
+    res.send("hello restify")
+});
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
