2b78aef24502d1ec7863289ebec39792ccd56fab
belyi <belyi@greendatasoft.ru>
2017-09-21 01:25:56 +0500

    refactor

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,11 +1,8 @@
 import * as mongoose from "mongoose";
 
 export function initDb() {
-  mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/botDb");
-  var db = mongoose.connection;
+  mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/botDb", {useMongoClient: true}).then(() =>
+    console.log("connected to db"));
+  const db = mongoose.connection;
   db.on("error", console.error.bind(console, "connection error:"));
-  db.once("open", function () {
-    console.log("connected to db");
-    // we're connected!
-  });
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,14 +1,19 @@
-import {createServer} from "restify";
 import {ChatConnector} from "botbuilder";
+import * as express from "express";
 
 export function initServer(botConnector: ChatConnector) {
-  const server = createServer();
-  server.listen(process.env.port || process.env.PORT || 8080, function () {
-    console.log("%s listening to %s", server.name, server.url);
-  });
+  const server = express();
+
+
 
   server.post("/api/messages", botConnector.listen());
   server.get("/", (req, res) => {
     res.send("hello restify");
+  });
+
+  const port = process.env.port || process.env.PORT || 8080;
+
+  server.listen(port, function () {
+    console.log(" listening on %s", port);
   });
 }
